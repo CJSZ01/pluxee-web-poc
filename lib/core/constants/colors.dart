86c98651bf73cbd5d2b6b95dab044af303abd4a8
Caio Zubek <zubek.caio@gmail.com>
2025-04-09 11:00:39 -0300

    feat: Implemented initial design system

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+final class AppColors {
+  AppColors._();
+  static Color get primary => Color(0xFF00EB5E);
+  static Color get secondary => Color(0xFF221C46);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:pluxee_web_poc/presentation/pages/signup/login_page.dart';
 import 'package:provider/provider.dart';
 
 import 'data/datasources/get_dog_image_datasource.dart'
@@ -7,7 +8,7 @@ import 'data/datasources/get_dog_image_datasource.dart'
 import 'data/repositories/get_dog_image_repository.dart'
     show GetDogImageRepository;
 import 'domain/usecases/get_dog_image_usecase.dart' show GetDogImageUsecase;
-import 'presentation/pages/dog_image_page.dart';
+
 import 'presentation/providers/dog_provider.dart';
 
 void main() {
@@ -19,9 +20,9 @@ void main() {
           (_) =>
               DogProvider(getDogImageUsecase: GetDogImageUsecase(repository)),
       child: MaterialApp(
-        title: 'Test Live Code',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: DogImagePage(),
+        title: 'Pluxee Web POC',
+        theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'plx'),
+        home: LoginPage(),
       ),
     ),
   );
